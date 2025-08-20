6ea281400e181fd9ec7d8fd74f0c4a6d8a38abf5
VennelaNalajala <vennela.nalajala@kasmodigital.com>
2025-08-20 16:00:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,8 @@ st.write(
 cnx=st.connection("snowflake")
 session=cnx.session()
 
-
+name_on_order = st.text_input("Name on smoothie")
+st.write("the name on your smoothie will be", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
@@ -19,8 +20,7 @@ st.dataframe(pd_df)
 st.stop()
 
 
-name_on_order = st.text_input("Name on smoothie")
-st.write("the name on your smoothie will be", name_on_order)
+
 
 
 
